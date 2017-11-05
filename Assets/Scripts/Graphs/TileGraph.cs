c5f59d5cbcd15c85197cf75d629aad2ca6c3ed40
Shane <shanemcdermott@unomaha.edu>
2017-11-05 15:47:29 -0600

    Added Rotated Map to show invalid blockage, changed tileSize to Vector3

--- Assets/Scripts/Graphs/TileGraph.cs
+++ Assets/Scripts/Graphs/TileGraph.cs
@@ -7,7 +7,7 @@ public class TileGraph : MonoBehaviour, IGraph<int>
 {
     public bool drawGizmos = false;
 
-    public float tileSize = 1.0f;
+    public Vector3 tileSize = new Vector3(1.0f, 0.5f, 1.0f);
     public Vector3 extents = new Vector3(20,1,20);
 
     private List<TileBox> tiles;
@@ -18,19 +18,21 @@ public class TileGraph : MonoBehaviour, IGraph<int>
         throw new NotImplementedException();
     }
 
+
+
     public void OnDrawGizmos()
     {
         if (!drawGizmos) return;
-        if (tileSize == 0 || extents.x == 0 || extents.y == 0 || extents.z == 0) return;
+        if (tileSize.x ==0 || tileSize. y == 0 || tileSize.z == 0 || extents.x == 0 || extents.y == 0 || extents.z == 0) return;
 
         Vector2 min = new Vector2(transform.position.x - extents.x, transform.position.z - extents.z);
         Vector2 max = new Vector2(transform.position.x + extents.x, transform.position.z + extents.z);
-        Vector3 size = new Vector3(tileSize, tileSize, tileSize);
         Vector3 center = transform.position;
-        Vector3 boxExtents = new Vector3(tileSize * 0.5f, tileSize * 0.5f, tileSize * 0.5f);
-        for (center.x = min.x; center.x <= max.x; center.x += tileSize)
+        Vector3 boxExtents = new Vector3(0.5f, 0.5f,0.5f);
+        boxExtents.Scale(tileSize);
+        for (center.x = min.x; center.x <= max.x; center.x += tileSize.x)
         {
-            for (center.z = min.y; center.z <= max.y; center.z += tileSize)
+            for (center.z = min.y; center.z <= max.y; center.z += tileSize.z)
             {
                 Collider[] hits = Physics.OverlapBox(center, boxExtents);
                 if (hits.Length == 0)
@@ -38,10 +40,10 @@ public class TileGraph : MonoBehaviour, IGraph<int>
                 else
                     Gizmos.color = Color.red;
 
-                Gizmos.DrawCube(center, size);
+                Gizmos.DrawCube(center, tileSize);
 
                 Gizmos.color = Color.black;
-                Gizmos.DrawWireCube(center, size);
+                Gizmos.DrawWireCube(center, tileSize);
             }
 
         }
